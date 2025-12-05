cfda7292792ee6e189fac1a141bac8eb6febda6e
bronxpostma13 <bronxpostma@gmail.com>
2025-12-05 09:59:13 +0100

    comments

--- talent.js
+++ talent.js
@@ -1,6 +1,9 @@
 
+//  Select de button
 var button = document.querySelectorAll("button");
+// Voor elke button in het bestand 
 button.forEach(function(button){
+// Event toegevoegd en toggle
 button.addEventListener("click", function() {
   button.classList.toggle("yay");
 });
