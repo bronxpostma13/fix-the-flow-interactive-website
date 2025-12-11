b3ca045d1a0c10e7872ec13756f12328e0b2641c
bronxpostma13 <bronxpostma@gmail.com>
2025-12-11 22:13:35 +0100

    Hamburger menu toegevoegd met js

--- talent.js
+++ talent.js
@@ -1,10 +1,24 @@
 
 //  Select de button
-var button = document.querySelectorAll("button");
-// Voor elke button in het bestand 
+var button = document.querySelectorAll(".heart");
+// Voor elke button in het bestand met de class heart
 button.forEach(function(button){
 // Event toegevoegd en toggle
 button.addEventListener("click", function() {
   button.classList.toggle("yay");
 });
 })
+
+
+//  Select de buttons
+var hamburgerOpen = document.querySelector(".menu-toggle");
+var hamburgerClose = document.querySelector(".close-btn");
+var navBox = document.querySelector("#mobile-nav-box");
+
+hamburgerOpen.addEventListener("click", function() {
+  navBox.classList.add("open");
+});
+
+hamburgerClose.addEventListener("click", function() {
+  navBox.classList.remove("open");
+});
